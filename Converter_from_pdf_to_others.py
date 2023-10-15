1dcebcf5c18fab2beb091f8a08b0c2baf2254389
アンドリュー <bitweal12@gmail.com>
2023-10-16 00:05:50 +0300

    pdf_to_xlsx only all file

--- Converter_from_pdf_to_others.py
+++ Converter_from_pdf_to_others.py
@@ -1,5 +1,4 @@
 ﻿from pdf2docx import Converter
-import tabula
 from pptx import Presentation
 from pdf2image import convert_from_path
 import pdfplumber
@@ -8,11 +7,15 @@ import pandas as pd
 
 def pdf_to_docx(pdf_file, output_file, page=None, start_page=0, end_page=None):
     cv = Converter(pdf_file)
-    if page is not None:   
-        cv.convert(output_file, pages=page)       
-    else:       
-        cv.convert(output_file, start=start_page, end=end_page)
-    cv.close()
+    try:
+        if page is not None:   
+            cv.convert(output_file, pages=page)       
+        else:       
+            cv.convert(output_file, start=start_page, end=end_page)
+        print(f'Done: {output_file}')
+        cv.close()
+    except:
+        cv.close()
 
 
 def pdf_to_xlsx(pdf_file, xlsx_file, page="all"):
@@ -20,18 +23,19 @@ def pdf_to_xlsx(pdf_file, xlsx_file, page="all"):
     pdf = pdfplumber.open(pdf_file)
     excel_data = []
 
-    for page in pdf.pages:
-        table = page.extract_table()
+    tables = [page.extract_table() for page in pdf.pages]
+
+    for table in tables:
         if table:
-            for row in table:
-                excel_data.append(row)
+            excel_data.extend(table)
+            excel_data.append([])
 
     df = pd.DataFrame(excel_data)
 
     df.to_excel(xlsx_file, index=False, header=False)
 
-    print(f'PDF успешно сконвертирован в Excel: {xlsx_file}')
-
+    print(f'Done: {xlsx_file}')
+    
 
 def pdf_to_pptx(pdf_file, pptx_file, page):
     pass
@@ -50,32 +54,45 @@ def convert_file(pdf_file, output_file, choice_type, page, start_page, end_page,
         pdf_to_xlsx(pdf_file, output_file, page)
              
 
-def main():
-    pdf_file = input("Enter path and file name: ")
-    choice_type = input("Choose an option:\n1. Convert to DOCX/DOC\n2. Convert to XLSX\n3. Convert to PPTX\n4. Convert to Images\n")
-    if choice_type == "1":
-        doc_type = input("Enter doc/docx: ")
-    number_of_pages = input("Choose an option:\n1. Page range\n2. Only one page\n3. All file\n")
+def choice_page(number_of_pages):
     start_page = None
     end_page = None
     page = None
-    doc_type = None
-    
     if number_of_pages == "1":
         start_page = str(int(input("Start: ")) - 1)
         end_page = str(int(input("End: ")) - 1)
+        return start_page, end_page, None
     elif number_of_pages == "2":
         page = [str(int(input("Page: ")) - 1)] 
+        return None, None, page
     else:
-        start_page = 0
-        
-    output_file = input("Enter the path where you want to save the file: ")  
+        return None, 0, None
+    
+
+def output_file_preparation(pdf_file, output_file):
     parts = pdf_file.split('/') 
     file_name = parts[-1].split(".")
     if output_file == "":
         output_file += file_name[0]
     else:
         output_file += "/" + file_name[0]
+        
+    return output_file
+
+
+def main():
+    pdf_file = input("Enter path and file name: ")
+    choice_type = input("Choose an option:\n1. Convert to DOCX/DOC\n2. Convert to XLSX\n3. Convert to PPTX\n4. Convert to Images\n")
+    if choice_type == "1":
+        doc_type = input("Enter doc/docx: ")
+    number_of_pages = input("Choose an option:\n1. Page range\n2. Only one page\n3. All file\n")
+    doc_type = None
+    
+    start_page, end_page, page = choice_page(number_of_pages)
+        
+    output_file = input("Enter the path where you want to save the file: ") 
+    output_file = output_file_preparation(pdf_file, output_file)
+    
     convert_file(pdf_file, output_file, choice_type, page,  start_page, end_page, doc_type)
     
 
