530973b6eded51c16e1b34eaa03c1a156a444fcc
アンドリュー <bitweal12@gmail.com>
2023-10-16 18:38:06 +0300

    all work need interface

--- Converter_from_pdf_to_others.py
+++ Converter_from_pdf_to_others.py
@@ -9,23 +9,35 @@ import fitz
 from PIL import Image
 
 
-def pdf_to_docx(pdf_file, output_file, page=None, start_page=0, end_page=None):
+def pdf_to_docx(pdf_file, output_file, page):
     cv = Converter(pdf_file)
     try:
-        if page is not None:   
-            cv.convert(output_file, pages=page)       
-        else:       
+        if page is None:   
+            start_page, end_page = 0, None
             cv.convert(output_file, start=start_page, end=end_page)
+        elif len(page) == 2:
+            start_page, end_page = page[0], page[1]
+            cv.convert(output_file, start=start_page, end=end_page)
+        else:          
+            cv.convert(output_file, pages=page)    
+            
         print(f'Done: {output_file}')
         cv.close()
-    except:
+    except Exception as e:
+        print(f'An error occurred: {str(e)}')
         cv.close()
 
 
-def pdf_to_xlsx(pdf_file, xlsx_file, page_range=None):
+def pdf_to_xlsx(pdf_file, xlsx_file, page_range):
     xlsx_file += ".xlsx"
-    if page_range is not None:
+    if page_range is None:   
+        pass
+    elif len(page_range) == 2:
+        page_range = [int(page) for page in page_range]
+        page_range = list(range(page_range[0], page_range[1] + 1)) 
+    else:          
         page_range = [int(page) + 1 for page in page_range]
+               
     with pdfplumber.open(pdf_file) as pdf:
         all_data = []
         for page_number, page in enumerate(pdf.pages):
@@ -56,12 +68,16 @@ def pdf_to_xlsx(pdf_file, xlsx_file, page_range=None):
             print(f'Done: {xlsx_file}')
         else:
             print("No data found.")
+  
 
-    
-
-def pdf_to_pptx(pdf_file, pptx_file, page_range=None):
+def pdf_to_pptx(pdf_file, pptx_file, page_range):
     pptx_file += ".pptx"
-    if page_range is not None:
+    if page_range is None:   
+        pass
+    elif len(page_range) == 2:
+        page_range = [int(page) for page in page_range]
+        page_range = list(range(page_range[0], page_range[1] + 1)) 
+    else:          
         page_range = [int(page) + 1 for page in page_range]
         
     prs = Presentation()
@@ -96,36 +112,40 @@ def pdf_to_pptx(pdf_file, pptx_file, page_range=None):
 
 
 def pdf_to_images(pdf_file, image_folder, page_range=None):
-    image_folder += ".png"
     if not os.path.exists(image_folder):
         os.mkdir(image_folder)
 
     with fitz.open(pdf_file) as pdf_document:
-        for page_number, pdf_page in enumerate(pdf_document):
-            if page_range is not None and page_number + 1 not in page_range:
-                continue
+        num_pages = len(pdf_document)
+
+        if page_range is None:
+            page_range = range(1, num_pages)
+        elif len(page_range) == 2:
+            page_range = range(int(page_range[0]), int(page_range[1]))
+          
+
+        for page_number in page_range:
+            pdf_page = pdf_document[int(page_number)]
 
             img = pdf_page.get_pixmap(matrix=fitz.Matrix(300/72, 300/72))
-
             img_bytes = img.samples
-            
+
             image = Image.frombytes("RGB", [img.width, img.height], img_bytes)
-            image_file = os.path.join(image_folder, f"page_{page_number + 1}.png")
+            image_file = os.path.join(image_folder, f"page_{int(page_number)+1}.png")
             image.save(image_file, "PNG")
 
-    print(f'Images saved to: {image_folder}')
+    print(f'Done: {image_folder}')
         
 
-def convert_file(pdf_file, output_file, choice_type, page, start_page, end_page, doc_type):
+def convert_file(pdf_file, output_file, choice_type, page, doc_type):
     if choice_type == "1":
-        pdf_to_docx(pdf_file, output_file + "." + doc_type, page, start_page, end_page)
+        pdf_to_docx(pdf_file, output_file + "." + doc_type, page)
     elif choice_type == "2":
         pdf_to_xlsx(pdf_file, output_file, page)
     elif choice_type == "3":
         pdf_to_pptx(pdf_file, output_file, page)
     elif choice_type == "4":
         pdf_to_images(pdf_file, output_file, page)
-        print(page)
              
 
 def choice_page(number_of_pages):
@@ -134,13 +154,14 @@ def choice_page(number_of_pages):
     page = None
     if number_of_pages == "1":
         start_page = str(int(input("Start: ")) - 1)
-        end_page = str(int(input("End: ")) - 1)
-        return start_page, end_page, None
+        end_page = str(input("End: "))
+        page = [start_page, end_page]
+        return page
     elif number_of_pages == "2":
         page = [str(int(input("Page: ")) - 1)] 
-        return None, None, page
+        return page
     else:
-        return None, 0, None
+        return page
     
 
 def output_file_preparation(pdf_file, output_file):
@@ -157,17 +178,17 @@ def output_file_preparation(pdf_file, output_file):
 def main():
     pdf_file = input("Enter path and file name: ")
     choice_type = input("Choose an option:\n1. Convert to DOCX/DOC\n2. Convert to XLSX\n3. Convert to PPTX\n4. Convert to Images\n")
+    doc_type = None
     if choice_type == "1":
         doc_type = input("Enter doc/docx: ")
     number_of_pages = input("Choose an option:\n1. Page range\n2. Only one page\n3. All file\n")
-    doc_type = None
     
-    start_page, end_page, page = choice_page(number_of_pages)
+    page = choice_page(number_of_pages)
         
     output_file = input("Enter the path where you want to save the file: ") 
     output_file = output_file_preparation(pdf_file, output_file)
     
-    convert_file(pdf_file, output_file, choice_type, page,  start_page, end_page, doc_type)
+    convert_file(pdf_file, output_file, choice_type, page, doc_type)
     
 
 if __name__ == "__main__":
