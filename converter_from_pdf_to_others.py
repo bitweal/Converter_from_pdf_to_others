0b4c8d46be1a6f7eeca2b7f5ce5f547cfb940c5d
アンドリュー <bitweal12@gmail.com>
2023-10-19 19:16:12 +0300

    v.1.1.4

--- converter_from_pdf_to_others.py
+++ converter_from_pdf_to_others.py
@@ -12,19 +12,23 @@ import sys
 def pdf_to_docx(pdf_file, output_file, page):
     cv = Converter(pdf_file)
     try:
-        if page is None:   
-            start_page, end_page = 0, None
-            cv.convert(output_file, start=start_page, end=end_page)
+        cv.start_conversion(output_file)
+        if page is None:
+            start_page, end_page = 0, cv.page_count
         elif len(page) == 2:
             start_page, end_page = page[0], page[1]
-            cv.convert(output_file, start=start_page, end=end_page)
-        else:          
-            cv.convert(output_file, pages=page)    
-            
+        else:
+            start_page, end_page = page[0], page[0] + 1
+
+        for page_num in range(start_page, end_page):
+            page = cv.get_page(page_num)
+            cv.add_page(page)
+
+        cv.finish_conversion()
         print(f'Done: {output_file}')
-        cv.close()
     except Exception as e:
         print(f'An error occurred: {str(e)}')
+    finally:
         cv.close()
 
 
@@ -36,36 +40,37 @@ def pdf_to_xlsx(pdf_file, xlsx_file, page_range):
         page_range = list(range(page_range[0], page_range[1])) 
     else:          
         page_range = [int(page) for page in page_range]
-    with pdfplumber.open(pdf_file) as pdf:
-        all_data = []
-        for page_number, page in enumerate(pdf.pages):
-            if page_range is not None and page_number not in page_range:
-                continue
-            tables = page.extract_tables()
-            for table_number, table in enumerate(tables):
-                if not table:
+    try:
+        with pdfplumber.open(pdf_file) as pdf:
+            all_data = []
+            for page_number, page in enumerate(pdf.pages):
+                if page_range is not None and page_number not in page_range:
                     continue
+                tables = page.extract_tables()
+                for table_number, table in enumerate(tables):
+                    if not table:
+                        continue
                 
-                df = pd.DataFrame(table)
+                    df = pd.DataFrame(table)
                 
-                if df.empty:
-                    continue
+                    if df.empty:
+                        continue
 
-                all_data.append(df)
+                    all_data.append(df)
 
-        if page_range is None and all_data:
-            with pd.ExcelWriter(xlsx_file, engine='xlsxwriter') as writer:
-                for i, df in enumerate(all_data):
-                    df.to_excel(writer, sheet_name=f"Table_{i+1}", index=False, header=False)
-            print(f'Done: {xlsx_file}')
+            if page_range is None and all_data:
+                with pd.ExcelWriter(xlsx_file, engine='xlsxwriter') as writer:
+                    for i, df in enumerate(all_data):
+                        df.to_excel(writer, sheet_name=f"Table_{i+1}", index=False, header=False)
+                print(f'Done: {xlsx_file}')
             
-        elif page_range is not None and all_data:
-            with pd.ExcelWriter(xlsx_file, engine='xlsxwriter') as writer:
-                for i, df in enumerate(all_data):
-                    df.to_excel(writer, sheet_name=f"Table_{i+1}", index=False, header=False)
-            print(f'Done: {xlsx_file}')
-        else:
-            print("No data found.")
+            elif page_range is not None and all_data:
+                with pd.ExcelWriter(xlsx_file, engine='xlsxwriter') as writer:
+                    for i, df in enumerate(all_data):
+                        df.to_excel(writer, sheet_name=f"Table_{i+1}", index=False, header=False)
+                print(f'Done: {xlsx_file}')
+    except Exception as e:
+        print(f'An error occurred: {str(e)}')
   
 
 def pdf_to_pptx(pdf_file, pptx_file, page_range):
@@ -78,59 +83,62 @@ def pdf_to_pptx(pdf_file, pptx_file, page_range):
         page_range = [int(page) + 1 for page in page_range]
         
     prs = Presentation()
+    try:
+        with fitz.open(pdf_file) as pdf_document:
+            for page_number, pdf_page in enumerate(pdf_document):
+                if page_range is not None and page_number + 1 not in page_range:
+                    continue
 
-    with fitz.open(pdf_file) as pdf_document:
-        for page_number, pdf_page in enumerate(pdf_document):
-            if page_range is not None and page_number + 1 not in page_range:
-                continue
+                pdf_page_rect = pdf_page.rect
+                pdf_page_width = pdf_page_rect[2] - pdf_page_rect[0]
+                pdf_page_height = pdf_page_rect[3] - pdf_page_rect[1]
 
-            pdf_page_rect = pdf_page.rect
-            pdf_page_width = pdf_page_rect[2] - pdf_page_rect[0]
-            pdf_page_height = pdf_page_rect[3] - pdf_page_rect[1]
+                slide = prs.slides.add_slide(prs.slide_layouts[6]) 
+                slide_width = Inches(pdf_page_width / 72.0)
+                slide_height = Inches(pdf_page_height / 72.0)
+                prs.slide_width = slide_width
+                prs.slide_height = slide_height
 
-            slide = prs.slides.add_slide(prs.slide_layouts[6]) 
-            slide_width = Inches(pdf_page_width / 72.0)
-            slide_height = Inches(pdf_page_height / 72.0)
-            prs.slide_width = slide_width
-            prs.slide_height = slide_height
+                img = pdf_page.get_pixmap(matrix=fitz.Matrix(300/72, 300/72))
+                img_path = f"temp_image.png"
+                img.save(img_path)
 
-            img = pdf_page.get_pixmap(matrix=fitz.Matrix(300/72, 300/72))
-            img_path = f"temp_image.png"
-            img.save(img_path)
+                left = Inches(0)
+                top = Inches(0)
+                slide.shapes.add_picture(img_path, left, top, width=slide_width, height=slide_height)
 
-            left = Inches(0)
-            top = Inches(0)
-            slide.shapes.add_picture(img_path, left, top, width=slide_width, height=slide_height)
+                os.remove(img_path)
 
-            os.remove(img_path)
-
-    prs.save(pptx_file)
-    print(f'Done: {pptx_file}')
-
+        prs.save(pptx_file)
+        print(f'Done: {pptx_file}')
+    except Exception as e:
+        print(f'An error occurred: {str(e)}')
 
 def pdf_to_images(pdf_file, image_folder, page_range=None):
     if not os.path.exists(image_folder):
         os.mkdir(image_folder)
+    try:
+        with fitz.open(pdf_file) as pdf_document:
+            num_pages = len(pdf_document)
 
-    with fitz.open(pdf_file) as pdf_document:
-        num_pages = len(pdf_document)
+            if page_range is None:
+                page_range = range(0, num_pages)
+            elif len(page_range) == 2:
+                page_range = range(int(page_range[0]), int(page_range[1]))
 
-        if page_range is None:
-            page_range = range(0, num_pages)
-        elif len(page_range) == 2:
-            page_range = range(int(page_range[0]), int(page_range[1]))
+            for page_number in page_range:
+                pdf_page = pdf_document[int(page_number)]
 
-        for page_number in page_range:
-            pdf_page = pdf_document[int(page_number)]
+                img = pdf_page.get_pixmap(matrix=fitz.Matrix(300/72, 300/72))
+                img_bytes = img.samples
 
-            img = pdf_page.get_pixmap(matrix=fitz.Matrix(300/72, 300/72))
-            img_bytes = img.samples
+                image = Image.frombytes("RGB", [img.width, img.height], img_bytes)
+                image_file = os.path.join(image_folder, f"page_{int(page_number)+1}.jpg")
+                image.save(image_file, "JPEG")
 
-            image = Image.frombytes("RGB", [img.width, img.height], img_bytes)
-            image_file = os.path.join(image_folder, f"page_{int(page_number)+1}.jpg")
-            image.save(image_file, "JPEG")
-
-    print(f'Done: {image_folder}')
+        print(f'Done: {image_folder}')
+    except Exception as e:
+        print(f'An error occurred: {str(e)}')
         
 
 def convert_file(pdf_file, output_file, page):
